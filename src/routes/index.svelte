837d3558ad97154739c7fafc1911adb16c75fe4e
Steven Boutcher <boutchersj@gmail.com>
2022-08-01 06:31:15 -0500

    Add image files; replace long bio with shorter one; base-level font-size styling in Tailwind; copy the education section content over from the old project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="flex flex-col items-center">
+    <h1 class="text-4xl">Steven Boutcher</h1>
+    <h2 class="text-xl">Engineer of Websites, Docs, Tests, and Sweet Parkour Moves</h2>
+</header>
+
+<h3 class="text-xl">About Me</h3>
+<section>
+    <p>
+        For those interested in the long version, go 
+        <a href="https://blog.stevenboutcher.com/my-tech-career-origin-story-part-i">
+            here
+        </a>
+    </p>
+    <p>
+        For the rest of you, hi, I'm Steven! Thanks for stopping by!
+        I made the decision in 2017 to learn web development in my spare time and now I'm an SDET at Fetch Rewards. 
+        Between then and now, I've met the love of my life and married her, been a dog dad, a cat dad, traveled overseas and domestically, and lots of other stuff! 
+        Basically, my story is one of a guy who:
+    </p>
+        <ul>
+            <li>loves to learn new things and experience the world</li>
+            <li>gets to experience the joys & sorrows of clinically diagnosed ADHD</li>
+            <li>came from a middle-class home in a small city in Wisconsin</li>
+            <li>enjoys pickled jalapeños almost more than cheese</li>
+            <li>worked a lot of thankless customer service jobs before deciding he could do better</li>
+            <li>is always looking for ways to make life easier for others (and himself)</li>
+            <li>switched his college major 5 times and still chose the wrong one</li>
+            <li>isn't buying any of the lies about coffee being bad for you</li>
+        </ul>
+    <p>
+        If you'd like to chat over coffee over Zoom, drop me an 
+        <a href="mailto:boutchersj@gmail.com">
+            email
+        </a>.
+    </p>
+</section>
+
+<h3 class="text-xl">Education</h3>
+<section>
+    <h4 class="text-lg">University of Wisconsin - Whitewater</h4>
+    <h4 class="text-lg">Spanish, BSE (Education)</h4>
+    <div>
+        <img src="src/images/bse_spanish_ed.png" alt="Bachelors degree diploma" />
+    </div>
+    <h4 class="text-lg">QA Automation</h4>
+    <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.codecademy.com/profiles/parkoursteve/certificates/1c05e0382bc5681c824c4cbe85c126fd">
+            CodeCademy – Ruby
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/certificate/UC-501f007b-cf5a-4299-84f8-7309d0add1c7/">
+            Udemy – RSpec
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://fetchrewards.udemy.com/certificate/UC-b96c7643-85bb-4ae6-a34c-e293e39bcba6/">
+            Udemy – Python
+        </a>
+    </div>
+    <h4 class="text-lg">Functional QA</h4>
+    <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://atsqa.org/certified-testers/profile/cfc87a81d4ee464b9f8c702a6ffeb8dd">
+            ASTQB - Foundation Level Agile Tester
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://bit.ly/3vsbF06">
+            LinkedIn Learning – SQL for Testers
+        </a>
+    </div>
+    <h4 class="text-lg">Web Development</h4>
+    <div>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/certification/boutchersj/responsive-web-design">
+            freeCodeCamp – Responsive Web Design
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/certification/boutchersj/javascript-algorithms-and-data-structures">
+            freeCodeCamp – JavaScript Algorithms & Data Structures
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/certification/boutchersj/front-end-development-libraries">
+            freeCodeCamp – Front End Development Libraries
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://scrimba.com/certificate/uBqQNau9/gresponsive">
+            Scrimba – Learn Responsive Web Design
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/certificate/UC-4f7e780a-2739-4cc6-b1d5-ad89d606f678/">
+            Udemy – NodeJS
+        </a>
+    </div>
+</section>
