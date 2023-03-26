67cb3a6155056175197becb86c91acab544842c1
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-03-25 22:46:01 -0500

    Put images folder inside static instead of src

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,6 @@
                 The world can only define you if you don't show anyone who you are.
             </p>
         </div>
-        <img src='src/images/parkour_3_16.gif' alt='GIF of me doing parkour' />
+        <img src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
     </div>
 </main>
