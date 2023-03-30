ee4a92115568d7d362a0a46554553b5aa2bced43
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-03-30 17:28:41 -0500

    Add years and order by earliest

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <main class='flex flex-col w-full'>
     <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-cyan-500 to-white p-10'>
-        <p class='flex text-center font-mono text-3xl leading-[2] h-full p-10'>
+        <p class='flex text-center text-3xl leading-[2] h-full p-10'>
             Nobody knows why we are here,
             <br/>
             and it doesn't matter.
@@ -22,39 +22,39 @@
             These are some of the games I've chosen to play:
         </p>
     </div>
-    <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-red-500 to-white p-10'>
-        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Parkour</h3>
-        <img class='h-full border border-white rounded-xl border-opacity-0 my-10' src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
-        <p class='font-mono w-full text-center'>Creative movement for efficient navigation</p>
-        <p class='w-full text-center py-5'>----------</p>
-        <p class='font-mono w-full text-center'>Bring out your inner monkey</p>
-        <div class='w-full flex justify-center py-10'>
-            <p class='w-[25%] text-center scale-[2]'>🐒</p>
-        </div>
-    </div>
     <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-orange-500 to-white p-10'>
-        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Violin</h3>
-        <p class='font-mono w-full text-center'>The musical instrument closest to the human voice</p>
-        <p class='w-full text-center py-5'>----------</p>
-        <p class='font-mono w-full text-center'>Deal with the devil not required to play</p>
+        <h3 class='text-2xl md:text-4xl w-full text-center my-5'>Violin</h3>
+        <p class='w-full text-center'>The musical instrument closest to the human voice</p>
+        <p class='w-full text-md md:text-xl text-center py-5'>- since 2004 -</p>
+        <p class='w-full text-center'>Deal with the devil not required to play</p>
         <div class='w-full flex justify-center py-10'>
             <p class='w-[25%] text-center scale-[2]'>🎻</p>
         </div>
     </div>
+    <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-red-500 to-white p-10'>
+        <h3 class='text-2xl md:text-4xl w-full text-center my-5'>Parkour</h3>
+        <img class='h-full border border-white rounded-xl border-opacity-0 my-10' src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
+        <p class='w-full text-center'>Creative movement for efficient navigation</p>
+        <p class='w-full text-md md:text-xl text-center py-5'>- since 2010 -</p>
+        <p class='w-full text-center'>Bring out your inner monkey</p>
+        <div class='w-full flex justify-center py-10'>
+            <p class='w-[25%] text-center scale-[2]'>🐒</p>
+        </div>
+    </div>
     <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-purple-500 to-white p-10'>
-        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Web Development</h3>
-        <p class='font-mono w-full text-center'>Making magic documents on the internet</p>
-        <p class='w-full text-center py-5'>----------</p>
-        <p class='font-mono w-full text-center'>*ahem* *points at this website*</p>
+        <h3 class='text-2xl md:text-4xl w-full text-center my-5'>Web Development</h3>
+        <p class='w-full text-center'>Making magic documents on the internet</p>
+        <p class='w-full text-md md:text-xl text-center py-5'>- since 2017 -</p>
+        <p class='w-full text-center'>*ahem* *points at this website*</p>
         <div class='w-full flex justify-center py-10'>
             <p class='w-[25%] text-center scale-[2]'>🖥️</p>
         </div>
     </div>
     <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-green-500 to-white p-10'>
-        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Software QA</h3>
-        <p class='font-mono w-full text-center'>Testing apps, websites, APIs, etc.</p>
-        <p class='w-full text-center py-5'>----------</p>
-        <p class='font-mono w-full text-center'>also, automating these tests 🤖</p>
+        <h3 class='text-2xl md:text-4xl w-full text-center my-5'>Software QA</h3>
+        <p class='w-full text-center'>Testing apps, websites, APIs, etc.</p>
+        <p class='w-full text-md md:text-xl text-center py-5'>- since 2020 -</p>
+        <p class='w-full text-center'>also, automating these tests 🤖</p>
         <div class='w-full flex justify-center py-10'>
             <p class='w-[25%] text-center scale-[2]'>🧪</p>
         </div>
