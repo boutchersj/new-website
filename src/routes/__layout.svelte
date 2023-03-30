b14b8abb85220e69baf95c66ab4c437e5842b6d4
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-03-30 13:52:29 -0500

    Further CSS cleanup

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,15 +13,15 @@
 </script>
 
 <header class='flex flex-col bg-black text-white text-lg p-5 justify-center items-center'>
-  <nav class='flex justify-between items-center w-full '>
+  <nav class='flex justify-between items-center w-full'>
     <h1 class='text-3xl'>Steven Boutcher</h1>
-    <button role='switch' on:click={toggleMenu} class='p-5 hover:bg-red-500 w-16'><img src='images/hamburger.png' alt='hamburger menu icon'/></button>
+    <button role='switch' on:click={toggleMenu} class='p-5 hover:bg-cyan-500 rounded-xl w-16'><img src='images/hamburger.png' alt='hamburger menu icon'/></button>
   </nav>
-  <div id='nav-menu' class='hidden h-[100vh] flex flex-col justify-evenly items-center'>
-    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.youtube.com/channel/UC3FmOiyig5g5J1MtJHj9XtQ">YouTube</a>
-    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://blog.stevenboutcher.com">Blog</a>
-    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.linkedin.com/in/boutchersj">LinkedIn</a>
-    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.twitter.com/StevenBoutcher">Twitter</a>
+  <div id='nav-menu' class='hidden h-[100vh] flex flex-col justify-start items-center'>
+    <a target='_blank' class='flex items-center justify-center w-[100vw] h-[25%] hover:bg-cyan-500' href="https://www.youtube.com/channel/UC3FmOiyig5g5J1MtJHj9XtQ">YouTube</a>
+    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[25%] hover:bg-cyan-500' href="https://blog.stevenboutcher.com">Blog</a>
+    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[25%] hover:bg-cyan-500' href="https://www.linkedin.com/in/boutchersj">LinkedIn</a>
+    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[25%] hover:bg-cyan-500' href="https://www.twitter.com/StevenBoutcher">Twitter</a>
 </div>
 </header>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,43 @@
 <main class='flex flex-col w-full'>
-    <div class='flex h-full bg-gradient-to-t from-red-500 to-gray-700'>
-        <p class='bg-white h-full font-mono text-3xl leading-[2] p-16 text-center flex flex-col justify-center opacity-30'>
-            The world can only define you if you don't show anyone who you are.
+    <div class='flex justify-center h-full bg-gradient-to-b from-black via-cyan-500 to-white p-10'>
+        <p class='bg-white h-full font-mono text-3xl leading-[2] p-16 text-center flex flex-col justify-center opacity-50 rounded-xl'>
+            Nobody knows why we are here,
+            <br/>
+            and it doesn't matter.
+            <br />
+            ~
+            <br />
+            The world is our playground,
+            <br />
+            and we must learn to play.
+            <br />
+            ~
+            <br />
+            If we don't choose our own game,
+            <br />
+            we will play someone else's game.
+            <br />
+            ~
+            <br />
+            These are some of the games I've chosen to play:
         </p>
     </div>
-    <div class='flex flex-col justify-evenly h-full bg-gradient-to-l from-red-500 to-gray-700'>
-        <p class='font-mono text-3xl w-full text-center mt-5'>Parkour</p>
-        <img class='h-full scale-[0.6] border border-white rounded-xl border-opacity-0' src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
-        <p class='font-mono text-lg w-full text-center px-10 pb-5'>Creative movement for efficient navigation</p>
-        <p class='w-full text-center pb-5'>----------</p>
-        <p class='font-mono text-lg w-full text-center px-10 pb-10'>Bring out your inner monkey</p>
-        <div class='w-full flex justify-center px-10 pb-10'>
+    <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-red-500 to-white p-10'>
+        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Parkour</h3>
+        <img class='h-full border border-white rounded-xl border-opacity-0 my-10' src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
+        <p class='font-mono w-full text-center'>Creative movement for efficient navigation</p>
+        <p class='w-full text-center py-5'>----------</p>
+        <p class='font-mono w-full text-center'>Bring out your inner monkey</p>
+        <div class='w-full flex justify-center py-10'>
             <p class='w-[25%] text-center scale-[2]'>🐒</p>
         </div>
     </div>
-    <div class='flex flex-col justify-evenly h-full bg-gradient-to-l from-orange-500 to-gray-700'>
-        <p class='font-mono text-3xl w-full text-center mt-5'>Violin</p>
-        <p class='font-mono text-lg w-full text-center px-10 pb-5'>The musical instrument closest to the human voice</p>
-        <p class='w-full text-center pb-5'>----------</p>
-        <p class='font-mono text-lg w-full text-center px-10 pb-10'>Deal with the devil not required to play</p>
-        <div class='w-full flex justify-center px-10 pb-10'>
+    <div class='flex flex-col text-lg md:text-2xl justify-center items-center h-full bg-gradient-to-b from-white via-orange-500 to-white p-10'>
+        <h3 class='font-mono text-2xl md:text-4xl w-full text-center my-5'>Violin</h3>
+        <p class='font-mono w-full text-center'>The musical instrument closest to the human voice</p>
+        <p class='w-full text-center py-5'>----------</p>
+        <p class='font-mono w-full text-center'>Deal with the devil not required to play</p>
+        <div class='w-full flex justify-center py-10'>
             <p class='w-[25%] text-center scale-[2]'>🎻</p>
         </div>
     </div>
