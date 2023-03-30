26eff65ebe0e678b6d5b127a43ab013119170c05
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-03-30 12:58:16 -0500

    Add a hamburger menu; add a violin section without a GIF; clean up styling for mobile-responsive UI

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,28 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  function toggleMenu(event: Event) {
+    const { target } = event
+    const isMenuHidden = document.getElementById('nav-menu')?.classList.contains('hidden')
+    if (isMenuHidden) {
+      document.getElementById('nav-menu')?.classList.remove('hidden')
+    }
+    else {
+      document.getElementById('nav-menu')?.classList.add('hidden')
+    }
+  }
 </script>
 
-<header class='flex justify-between bg-black text-white h-16 text-lg p-5 justify-center items-center'>
-  <h1 class='text-3xl'>Steven Boutcher</h1>
-  <nav class='underline flex w-[30%] justify-between'>
-    <a target='_blank' class='hover:text-cyan-500' href="https://www.youtube.com/channel/UC3FmOiyig5g5J1MtJHj9XtQ">YouTube</a>
-    <a target='_blank' class='hover:text-cyan-500' href="https://blog.stevenboutcher.com">Blog</a>
-    <a target='_blank' class='hover:text-cyan-500' href="https://www.linkedin.com/in/boutchersj">LinkedIn</a>
-    <a target='_blank' class='hover:text-cyan-500' href="https://www.twitter.com/StevenBoutcher">Twitter</a>
+<header class='flex flex-col bg-black text-white text-lg p-5 justify-center items-center'>
+  <nav class='flex justify-between items-center w-full '>
+    <h1 class='text-3xl'>Steven Boutcher</h1>
+    <button role='switch' on:click={toggleMenu} class='p-5 hover:bg-red-500 w-16'><img src='images/hamburger.png' alt='hamburger menu icon'/></button>
   </nav>
+  <div id='nav-menu' class='hidden h-[100vh] flex flex-col justify-evenly items-center'>
+    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.youtube.com/channel/UC3FmOiyig5g5J1MtJHj9XtQ">YouTube</a>
+    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://blog.stevenboutcher.com">Blog</a>
+    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.linkedin.com/in/boutchersj">LinkedIn</a>
+    <a target='_blank' class='w-36 hover:bg-red-500 rounded-xl p-5 text-center' href="https://www.twitter.com/StevenBoutcher">Twitter</a>
+</div>
 </header>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,26 @@
 <main class='flex flex-col w-full'>
-    <div class='flex h-[100vh] bg-gradient-to-t from-red-500 to-gray-700'>
-        <div class='h-full w-[50%]'>
-            <p class='bg-white h-full font-mono text-[48px] p-16 text-center flex flex-col justify-center opacity-30'>
-                The world can only define you if you don't show anyone who you are.
-            </p>
+    <div class='flex h-full bg-gradient-to-t from-red-500 to-gray-700'>
+        <p class='bg-white h-full font-mono text-3xl leading-[2] p-16 text-center flex flex-col justify-center opacity-30'>
+            The world can only define you if you don't show anyone who you are.
+        </p>
+    </div>
+    <div class='flex flex-col justify-evenly h-full bg-gradient-to-l from-red-500 to-gray-700'>
+        <p class='font-mono text-3xl w-full text-center mt-5'>Parkour</p>
+        <img class='h-full scale-[0.6] border border-white rounded-xl border-opacity-0' src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
+        <p class='font-mono text-lg w-full text-center px-10 pb-5'>Creative movement for efficient navigation</p>
+        <p class='w-full text-center pb-5'>----------</p>
+        <p class='font-mono text-lg w-full text-center px-10 pb-10'>Bring out your inner monkey</p>
+        <div class='w-full flex justify-center px-10 pb-10'>
+            <p class='w-[25%] text-center scale-[2]'>🐒</p>
         </div>
-        <img src='images/parkour_3_16.gif' alt='GIF of me doing parkour' />
+    </div>
+    <div class='flex flex-col justify-evenly h-full bg-gradient-to-l from-orange-500 to-gray-700'>
+        <p class='font-mono text-3xl w-full text-center mt-5'>Violin</p>
+        <p class='font-mono text-lg w-full text-center px-10 pb-5'>The musical instrument closest to the human voice</p>
+        <p class='w-full text-center pb-5'>----------</p>
+        <p class='font-mono text-lg w-full text-center px-10 pb-10'>Deal with the devil not required to play</p>
+        <div class='w-full flex justify-center px-10 pb-10'>
+            <p class='w-[25%] text-center scale-[2]'>🎻</p>
+        </div>
     </div>
 </main>
