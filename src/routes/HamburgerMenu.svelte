abe29039bacd446b711cc20eb1257157682635cd
Steven Boutcher <stevenboutcher@Stevens-MacBook-Air.local>
2023-04-06 12:25:58 -0500

    Change hamburger menu options; close the menu after a menu option is clicked

--- src/routes/HamburgerMenu.svelte
+++ src/routes/HamburgerMenu.svelte
@@ -19,8 +19,8 @@
     <button role='switch' on:click={toggleMenu} class='p-5 hover:bg-cyan-500 rounded-xl w-16'><img src='images/hamburger.png' alt='hamburger menu icon'/></button>
     </nav>
     <div id='nav-menu' class='hidden h-[100vh] flex flex-col justify-start items-center'>
-    <a target='_blank' class='flex items-center justify-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://www.youtube.com/channel/UC3FmOiyig5g5J1MtJHj9XtQ">YouTube</a>
-    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://blog.stevenboutcher.com">Blog</a>
-    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://www.linkedin.com/in/boutchersj">LinkedIn</a>
-    <a target='_blank' class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://www.twitter.com/StevenBoutcher">Twitter</a>
+    <a on:click={toggleMenu} class='flex items-center justify-center w-[100vw] h-[20%] hover:bg-cyan-500' href="/">Home</a>
+    <a on:click={toggleMenu} target='_blank' class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://linktr.ee/stevenboutcher">Contact</a>
+    <a on:click={toggleMenu} target='_blank' class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="https://blog.stevenboutcher.com">QA Blog</a>
+    <a on:click={toggleMenu} class='flex justify-center items-center w-[100vw] h-[20%] hover:bg-cyan-500' href="/blog">Parkour Blog</a>
 </div>
