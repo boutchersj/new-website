9bf69c685443fd7938f0d28d5ae37895b61bd51f
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-04-11 06:48:29 -0500

    Update weigh-in for Day 9

--- src/routes/blog/posts/9.svelte
+++ src/routes/blog/posts/9.svelte
@@ -2,7 +2,7 @@
     import BlogPost from '../BlogPost.svelte'
 </script>
 
-<BlogPost dayNum={9} date='April 11, 2023' weighIn={236.6}>
+<BlogPost dayNum={9} date='April 11, 2023' weighIn={233.6}>
     <div slot='content'>
         <p>
             [In progress]
