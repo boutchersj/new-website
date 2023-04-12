dc57a3c4a6e90daa2cbb644500b9b3aa80c415cb
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-04-12 05:46:36 -0500

    Add Day 9; adjust links to open in a new tab instead of replacing current tab

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -9,6 +9,7 @@
     import Day7 from './posts/7.svelte'
     import Day8 from './posts/8.svelte'
     import Day9 from './posts/9.svelte'
+    import Day10 from './posts/10.svelte'
 </script>
 
 <main class='flex flex-col w-[100vw] text-white'>
@@ -30,5 +31,6 @@
         <Day7 />
         <Day8 />
         <Day9 />
+        <Day10 />
     </section>
 </main>
---
+++ src/routes/blog/posts/10.svelte
@@ -0,0 +1,11 @@
+<script>
+    import BlogPost from '../BlogPost.svelte'
+</script>
+
+<BlogPost dayNum={10} date='April 12, 2023' weighIn={233}>
+    <div slot='content'>
+        <p>
+            [In progress]
+        </p>
+    </div>
+</BlogPost>
