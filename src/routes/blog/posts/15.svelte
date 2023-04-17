88b3c795a9f989c41672b31613d5786628623b43
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-04-17 09:22:51 -0500

    Update weight for Day 15

--- src/routes/blog/posts/15.svelte
+++ src/routes/blog/posts/15.svelte
@@ -2,7 +2,7 @@
     import BlogPost from '../BlogPost.svelte'
 </script>
 
-<BlogPost dayNum={15} date='April 17, 2023' weighIn={233.2}>
+<BlogPost dayNum={15} date='April 17, 2023' weighIn={232.8}>
     <div slot='content'>
         <p>
             [In progress]
