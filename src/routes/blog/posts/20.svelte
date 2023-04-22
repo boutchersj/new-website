bca965cc3b7d7bb2d44d95e622d047e33fe48e8e
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-04-21 21:34:52 -0500

    Add Day 19

---
+++ src/routes/blog/posts/20.svelte
@@ -0,0 +1,11 @@
+<script>
+    import BlogPost from '../BlogPost.svelte'
+</script>
+
+<BlogPost dayNum={20} date='April 22, 2023' weighIn={230.6}>
+    <div slot='content'>
+        <p>
+            [In progress]
+        </p>
+    </div>
+</BlogPost>
