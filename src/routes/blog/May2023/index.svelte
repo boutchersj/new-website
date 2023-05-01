272038231b102eaab8a9c9d3d4c62f2610b08e2c
Steven Boutcher <stevenboutcher@Stevens-MacBook-Air.local>
2023-05-01 09:29:33 -0500

    Add May 2023 template

---
+++ src/routes/blog/May2023/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '../../../app.css'
+    import DayJumper from '$lib/DayJumper.svelte'
+    import Day29 from '../../../blogPosts/parkour/28.svelte'
+</script>
+
+<main class='flex flex-col w-[100vw] text-white'>
+    <h1 class='w-full flex text-lg md:text-2xl justify-center items-center bg-red-500 p-10'>
+        <span class='p-7 rounded-xl bg-black flex flex-col justify-evenly items-center'>
+            <p class='text-red-500'>#NASTY Challenge</p>
+            <p class='font-bold'>April 2023</p>
+            <DayJumper endDay={28} />
+            <a class='text-sm' href='/blog'>Back to Blog</a>
+        </span>
+    </h1>
+    <section class='w-full flex flex-col items-center'>
+        <Day29 />
+    </section>
+</main>
