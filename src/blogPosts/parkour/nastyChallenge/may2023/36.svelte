d550cd5725bfdf92f533a68221d56165e1469fb6
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-05-09 06:59:48 -0500

    Start using the new component for Day 36 to test it out

--- src/blogPosts/parkour/nastyChallenge/may2023/36.svelte
+++ src/blogPosts/parkour/nastyChallenge/may2023/36.svelte
@@ -1,35 +1,28 @@
 <script>
     import BlogPost from '../../../../routes/blog/BlogPost.svelte'
+    import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
 </script>
 
 <BlogPost dayNum={36} date='May 8, 2023' weighIn={229.4}>
     <div slot='content'>
-        <h3 class='font-bold'>Food</h3>
-        <br />
-        <p>Under calorie goal by 200.</p>
-        <br />
-        <p>2 main meals:</p>
-        <ul class='list-disc ml-10 my-3'>
-            <li>McDonald's - steak egg & cheese bagel w/ a plain iced latte</li>
-            <li>EveryPlate - cream of mushroom soup</li>
-        </ul>
-        <p>Snacks - a salad I made at Trek's salad bar</p>
-
-        <br />
-
-        <h3 class='font-bold'>PT</h3>
-        <br />
-        <p>No</p>
-
-        <br />
-
-        <h3 class='font-bold'>Exercise</h3>
-        <br />
-        <p>Parkour-style conditioning:</p>
-        <ul class='list-disc ml-10 my-3'>
-            <li>quadrupedal movement</li>
-            <li>monkey bars</li>
-            <li>push-ups</li>
-        </ul>
+        <FoodExerciseLog
+            underGoal={true}
+            goalDelta={200}
+            mainMealsCount={2}
+            mealsList={[
+                "McDonald's - steak egg & cheese bagel w/ a plain iced latte",
+                "EveryPlate - cream of mushroom soup"
+            ]}
+            snacksList={[
+                "a salad I made at Trek's salad bar"
+            ]}
+            didPT={false}
+            exerciseSummary='Parkour-style conditioning'
+            exerciseList={[
+                'quadrupedal movement',
+                'monkey bars',
+                'push-ups'
+            ]}
+        />
     </div>
 </BlogPost>
