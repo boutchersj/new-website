c152cba2d523dc3efa097c2c5ea4a6668d462fc1
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-05-11 21:07:16 -0500

    Add Day 39

---
+++ src/blogPosts/parkour/nastyChallenge/40.svelte
@@ -0,0 +1,23 @@
+<!-- <script>
+    import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
+</script>
+
+<FoodExerciseLog
+    dayNum={40}
+    date='May 12, 2023'
+    weighIn={}
+    underGoal={}
+    goalDelta={}
+    mainMealsCount={}
+    mealsList={[
+        "",
+    ]}
+    snacksList={[
+        "",
+    ]}
+    didPT={}
+    exerciseSummary="
+    exerciseList={[
+        "",
+    ]}
+/> -->
--- src/blogPosts/parkour/nastyChallenge/may2023/39.svelte
+++ src/blogPosts/parkour/nastyChallenge/may2023/39.svelte
@@ -1,4 +1,4 @@
-<!-- <script>
+<script>
     import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
 </script>
 
@@ -6,18 +6,21 @@
     dayNum={39}
     date='May 11, 2023'
     weighIn={227.4}
-    underGoal={}
-    goalDelta={}
-    mainMealsCount={}
+    underGoal={false}
+    goalDelta={500}
+    mainMealsCount={3}
     mealsList={[
-        "",
+        "Sweet potato zucchini pancakes w/ mixed berries",
+        "Pretzel bun swiss cheeseburger & turkey wrap",
+        "Culver's - double deluxe cheeseburger w/o mayo"
     ]}
     snacksList={[
-        "",
+        "Starbucks - peach green tea lemonade",
+        "Starbucks - egg white, turkey bacon & white cheddar sandwich"
     ]}
-    didPT={}
-    exerciseSummary=""
+    didPT={false}
+    exerciseSummary="Trail riding"
     exerciseList={[
-        "",
+        "Mountain biking on the Trek trails",
     ]}
-/> -->
+/>
