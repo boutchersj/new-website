9b392d56ca0a130d25bb828f21f9fa1264a0a376
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-05-25 09:15:41 -0500

    Add the weight for Day 53

---
+++ src/blogPosts/parkour/nastyChallenge/may2023/53.svelte
@@ -0,0 +1,22 @@
+<!-- <script>
+    import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
+</script>
+
+<FoodExerciseLog
+    dayNum={53}
+    date='May 25, 2023'
+    weighIn={223.4}
+    underGoal={}
+    goalDelta={}
+    mainMealsCount={}
+    mealsList={[
+        "",
+    ]}
+    snacksList={[
+        "",
+    ]}
+    didPT={}
+    otherThoughts={`
+
+    `}
+/> -->
---
+++ src/blogPosts/parkour/nastyChallenge/may2023/54.svelte
@@ -0,0 +1,22 @@
+<!-- <script>
+    import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
+</script>
+
+<FoodExerciseLog
+    dayNum={54}
+    date='May 26, 2023'
+    weighIn={}
+    underGoal={}
+    goalDelta={}
+    mainMealsCount={}
+    mealsList={[
+        "",
+    ]}
+    snacksList={[
+        "",
+    ]}
+    didPT={}
+    otherThoughts={`
+
+    `}
+/> -->
