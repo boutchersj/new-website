cee04026e57414202ef0b91df76b86f464bce36c
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-05-30 07:24:43 -0500

    Add Day 57

--- src/blogPosts/parkour/nastyChallenge/may2023/57.svelte
+++ src/blogPosts/parkour/nastyChallenge/may2023/57.svelte
@@ -1,4 +1,4 @@
-<!-- <script>
+<script>
     import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
 </script>
 
@@ -6,22 +6,26 @@
     dayNum={57}
     date='May 29, 2023'
     weighIn={223.2}
-    underGoal={}
-    goalDelta={}
-    mainMealsCount={}
+    underGoal={false}
+    goalDelta={650}
+    mainMealsCount={2}
     mealsList={[
-        "",
+        "McDonald's - 20-piece chicken nuggets w/ sriracha mayo",
+        "Noodles & Company - 3-cheese tortelloni pesto w/ grilled chicken"
     ]}
     snacksList={[
-        '',
+        'String cheese',
+        'Hummus & carrots'
     ]}
-    didPT={}
-    exerciseSummary=''
+    didPT={true}
+    exerciseSummary='Summer fun'
     exerciseList={[
-        '',
+        'Biking to get smoothies (sugar-free)',
+        'Flying kites in the park',
+        'Attempting to run a mile, only got .13 miles before my knee flared up'
     ]}
     otherThoughts={`
         No clue why I went up 3 pounds and down 3 pounds again within 2 days. The human body is weird. Maybe 
         I should stop guessing and just keep doing what I'm doing until it starts to reverse my progress.
     `}
-/> -->
+/>
---
+++ src/blogPosts/parkour/nastyChallenge/may2023/58.svelte
@@ -0,0 +1,23 @@
+<!-- <script>
+    import FoodExerciseLog from '$lib/FoodExerciseLog.svelte';
+</script>
+
+<FoodExerciseLog
+    dayNum={58}
+    date='May 30, 2023'
+    weighIn={223.8}
+    underGoal={}
+    goalDelta={}
+    mainMealsCount={}
+    mealsList={[
+        "",
+    ]}
+    snacksList={[
+        '',
+    ]}
+    didPT={true}
+    exerciseSummary=''
+    exerciseList={[
+        '',
+    ]}
+/> -->
