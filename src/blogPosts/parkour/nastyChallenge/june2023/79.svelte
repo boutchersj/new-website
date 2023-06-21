c1656ea9b23197767bd2479f3ed06d9a65fd5719
Steven Boutcher <steven_boutcher@trekbikes.com>
2023-06-21 08:23:35 -0500

    Add Day 79 & 80

---
+++ src/blogPosts/parkour/nastyChallenge/june2023/79.svelte
@@ -0,0 +1,23 @@
+<script>
+    import ExerciseLog from '$lib/ExerciseLog.svelte';
+</script>
+
+<ExerciseLog
+    dayNum={79}
+    date='June 20, 2023'
+    weighIn={220.6}
+    didPT={true}
+    exerciseSummary='Mixed bag'
+    exerciseList={[
+        'Afternoon bike ride to YMCA & back',
+        '15-minute swim',
+        'Weightlifting - bench press, incline bench press, pull-ups'
+    ]}
+    fruitCups={1}
+    veggiesCups={4}
+    otherThoughts={`
+        My nutritionist & I agreed that it'd more productive to continue tracking food, but to reduce the granularity if 
+        I think it's bothersome to track in MyFitnessPal. So, I've added a feature to the new blog post format so I can track the 
+        number of cups of fruits & non-starchy veggies using emojis.
+    `}
+/>
---
+++ src/blogPosts/parkour/nastyChallenge/june2023/80.svelte
@@ -0,0 +1,17 @@
+<script>
+    import ExerciseLog from '$lib/ExerciseLog.svelte';
+</script>
+
+<ExerciseLog
+    dayNum={80}
+    date='June 21, 2023'
+    weighIn={223.2}
+    didPT={true}
+    exerciseSummary='Morning cardio'
+    exerciseList={[
+        'Morning bike ride to YMCA & back',
+        '15-minute swim (500 yds)',
+    ]}
+    fruitCups={2}
+    veggiesCups={0}
+/>
