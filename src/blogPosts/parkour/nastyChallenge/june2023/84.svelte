c4ebad4dc1f6cfac2db21a287a50d4f1c06e812b
Steven Boutcher <stevenboutcher@stevens-air.lan>
2023-06-25 16:29:52 -0500

    Add Day 84

---
+++ src/blogPosts/parkour/nastyChallenge/june2023/84.svelte
@@ -0,0 +1,12 @@
+<script>
+    import ExerciseLog from '$lib/ExerciseLog.svelte';
+</script>
+
+<ExerciseLog
+    dayNum={84}
+    date='June 25, 2023'
+    weighIn={219.6}
+    didPT={false}
+    fruitCups={2}
+    veggiesCups={4}
+/>
