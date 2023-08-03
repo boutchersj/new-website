e9342c059b176cbbaa30afcc73bc66c19bcd33a2
Steven Boutcher <steven_boutcher@trekbikes.com>
2023-08-03 09:10:01 -0500

    Add tentative Day 123

---
+++ src/blogPosts/parkour/nastyChallenge/august2023/123.svelte
@@ -0,0 +1,19 @@
+<script>
+    import ExerciseLog from '$lib/ExerciseLog.svelte';
+</script>
+
+<ExerciseLog
+    dayNum={123}
+    date='August 3, 2023'
+    weighIn={215.8}
+    didPT={false}
+    exerciseSummary='Mountain Biking?'
+    exerciseList={[
+        'Did I ride a bike today?'
+    ]}
+    fruitCups={2}
+    veggiesCups={4}
+    flax={true}
+    lGlutamine={true}
+    fishOil={true}
+/>
--- src/routes/blog/August2023/index.svelte
+++ src/routes/blog/August2023/index.svelte
@@ -3,6 +3,7 @@
     import MonthHeader from '$lib/MonthHeader.svelte'
     import Day121 from '../../../blogPosts/parkour/nastyChallenge/august2023/121.svelte'
     import Day122 from '../../../blogPosts/parkour/nastyChallenge/august2023/122.svelte'
+    // import Day123 from '../../../blogPosts/parkour/nastyChallenge/august2023/123.svelte'
 </script>
 
 <main class='flex flex-col w-[100vw] text-white'>
@@ -15,5 +16,6 @@
     <section class='w-full flex flex-col items-center'>
         <Day121 />
         <Day122 />
+        <!-- <Day123 /> -->
     </section>
 </main>
